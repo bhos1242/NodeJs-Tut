bdd7efc6919bfc394c624111643606b8779f1b74
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-10-26 21:04:04 +0530

    Refactor file handling code and add console.log statements

--- 05_File_Handling/file.js
+++ 05_File_Handling/file.js
@@ -1,19 +1,23 @@
-const fs = require("fs")
+const fs = require("fs");
 
 //Sync call
-fs.writeFileSync("./test.txt","Hey Vivek, How are you? I am Fine Thank you")
+// fs.writeFileSync("./test.txt","Hey Vivek, How are you? I am Fine Thank you")
 
-//Async
-fs.writeFile("./test2.txt","Hey vivek,, How are You?",(err)=>{})
+// //Async
+// fs.writeFile("./test2.txt","Hey vivek,, How are You?",(err)=>{})
 
-const contacts = fs.readFileSync("./contacts.txt","utf-8")
-console.log(contacts)
+// const contacts = fs.readFileSync("./contacts.txt","utf-8")
+// console.log(contacts)
 
-const contacts2 = fs.readFile("./contacts.txt","utf-8",(err,data)=>{
-    if(err){
-        console.log("Error",err)
-    }else{
-        console.log(data)
-    }
-})
-console.log(contacts2) //undefined
+// const contacts2 = fs.readFile("./contacts.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log("Error",err)
+//     }else{
+//         console.log(data)
+//     }
+// })
+// console.log(contacts2) //undefined
+
+fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+fs.cpSync("./test.txt", "./copy.txt");
